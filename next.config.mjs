7f9ccb69634d374a3e5e42b1e8c245bc885e54e4
Sergio López <sergiolopezgil123456@gmail.com>
2026-08-09 18:26:51 +0200

    fix: imagenes y readme cambiado

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 const nextConfig = {
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "www.gamerpower.com" },
+      { protocol: "https", hostname: "cdn1.epicgames.com" },
+      { protocol: "https", hostname: "cdn2.epicgames.com" },
+      { protocol: "https", hostname: "store-images.s-microsoft.com" },
+    ],
+  },
 };
 
 export default nextConfig;
