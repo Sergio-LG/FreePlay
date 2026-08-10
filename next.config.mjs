870d05c5e38c100b4c41a67f76e9a9c771b6fbfe
Sergio López <sergiolopezgil123456@gmail.com>
2026-08-10 17:02:07 +0200

    fix: la pagina es ahora responsive y se puede usar en dispositivos moviles

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 const nextConfig = {
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**.gamerpower.com" },
+      { protocol: "https", hostname: "**.epicgames.com" },
+      { protocol: "https", hostname: "**.cloudflare.steamstatic.com" },
+      { protocol: "https", hostname: "**.akamaihd.net" },
+    ],
+  },
 };
 
 export default nextConfig;
